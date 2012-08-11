3f42bc64fc2b097191336aabba1e0f69d1f73207
Chris Pilkington <chris.pilkington@gmail.com>
2012-08-11 23:40:33 +1000

    spitfire string Tidy up.

--- src/spitfire/util/string.cpp
+++ src/spitfire/util/string.cpp
@@ -33,56 +33,52 @@
 // Test strings
 // http://www.columbia.edu/kermit/utf8.html
 
-#define WHITE_SPACE "\t\v\r\n"
-#define LWHITE_SPACE L"\t\v\r\n"
-
-const char szWhiteSpace[] = {
-  0x09, // \t
-  0x0A, // \n
-  0x0B,
-  0x0C,
-  0x0D, // \r
-  0x20, // Space
-  0
-};
-
-const wchar_t wszWhiteSpace[] = {
-  0x0009, // <control-0009> to <control-000D>
-  0x000A,
-  0x000B,
-  0x000C,
-  0x000D,
-  0x0020, // Space
-  0x0085, // <control-0085>
-  0x00A0, // No-Break Space
-  0x1680, // Ogham Space Mark
-  0x180E, // Mongolian Vowel Separator
-  0x2000, // En Quad to Hair Space
-  0x2001,
-  0x2002,
-  0x2003,
-  0x2004,
-  0x2005,
-  0x2006,
-  0x2007,
-  0x2008,
-  0x2009,
-  0x200A,
-  0x200C, // Zero Width Non-Joiner
-  0x2028, // Line Separator
-  0x2029, // Paragraph Separator
-  0x202F, // Narrow No-Break Space
-  0x205F, // Medium Mathematical Space
-  0x3000, // Ideographic Space
-  0
-};
-
-
 namespace spitfire
 {
   namespace string
   {
+    #define WHITE_SPACE "\t\v\r\n"
+    #define LWHITE_SPACE L"\t\v\r\n"
 
+    const char szWhiteSpace[] = {
+      0x09, // \t
+      0x0A, // \n
+      0x0B,
+      0x0C,
+      0x0D, // \r
+      0x20, // Space
+      0
+    };
+
+    const wchar_t wszWhiteSpace[] = {
+      0x0009, // <control-0009> to <control-000D>
+      0x000A,
+      0x000B,
+      0x000C,
+      0x000D,
+      0x0020, // Space
+      0x0085, // <control-0085>
+      0x00A0, // No-Break Space
+      0x1680, // Ogham Space Mark
+      0x180E, // Mongolian Vowel Separator
+      0x2000, // En Quad to Hair Space
+      0x2001,
+      0x2002,
+      0x2003,
+      0x2004,
+      0x2005,
+      0x2006,
+      0x2007,
+      0x2008,
+      0x2009,
+      0x200A,
+      0x200C, // Zero Width Non-Joiner
+      0x2028, // Line Separator
+      0x2029, // Paragraph Separator
+      0x202F, // Narrow No-Break Space
+      0x205F, // Medium Mathematical Space
+      0x3000, // Ideographic Space
+      0
     };
 
 
