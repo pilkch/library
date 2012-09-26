7153b844a330ad024c8ab8d2ce5b10c4bb352f0e
Chris Pilkington <chris.pilkington@gmail.com>
2012-09-27 00:20:43 +1000

    spitfire endian We can now use __builtin_bswap16 under gcc to avoid doing our own bit shifting and masking.

--- include/spitfire/util/endian.h
+++ include/spitfire/util/endian.h
@@ -8,16 +8,14 @@ namespace spitfire
    namespace endian
    {
       #if defined(__WIN__) && !defined(COMPILER_GCC)
-      // TODO: Are these required?
       #pragma intrinsic(_byteswap_ushort)
       #pragma intrinsic(_byteswap_ulong)
       #pragma intrinsic(_byteswap_uint64)
-
       inline uint16_t Reverse(uint16_t value) { return _byteswap_ushort(u); }
       inline uint32_t Reverse(uint32_t value) { return _byteswap_ulong(u); }
       inline uint64_t Reverse(uint64_t value) { return _byteswap_uint64(u); }
       #else
-      inline uint16_t Reverse(uint16_t value) { return (((value & 0x00FF) << 8) | ((value & 0xFF00) >> 8)); }
+      inline uint16_t Reverse(uint16_t value) { return __builtin_bswap16(value); }
       inline uint32_t Reverse(uint32_t value) { return __builtin_bswap32(value); }
       inline uint64_t Reverse(uint64_t value) { return __builtin_bswap64(value); }
       #endif
