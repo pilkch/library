00eb9a61833742eca9038a0579de27bf0e95c3f1
Chris Pilkington <chris.pilkington@gmail.com>
2012-11-09 00:45:09 +1100

    breathe Using simpler application and state classes.

--- include/breathe/util/cApplication.h
+++ include/breathe/util/cApplication.h
@@ -1,6 +1,143 @@
 #ifndef CAPPLICATION_H
 #define CAPPLICATION_H
 
+// Standard headers
+#include <stack>
+
+// Spitfire headers
+#include <spitfire/math/math.h>
+
+// Breathe headers
+#include <breathe/audio/audio.h>
+#include <breathe/gui/cManager.h>
+#include <breathe/gui/cRenderer.h>
+#include <breathe/render/cContext.h>
+#include <breathe/render/cFont.h>
+#include <breathe/render/cSystem.h>
+#include <breathe/render/cVertexBufferObject.h>
+#include <breathe/render/cWindow.h>
+
+namespace breathe
+{
+  namespace util
+  {
+    class cApplication;
+
+    // ** cState
+
+    class cState : public gui::cWidgetEventListener
+    {
+    public:
+      explicit cState(cApplication& application);
+      virtual ~cState();
+
+      void Pause() { _OnPause(); }
+      void Resume() { _OnResume(); }
+
+      void OnWindowEvent(const gui::cWindowEvent& event) { _OnWindowEvent(event); }
+      void OnMouseEvent(const gui::cMouseEvent& event) { _OnMouseEvent(event); }
+      void OnKeyboardEvent(const gui::cKeyboardEvent& event) { _OnKeyboardEvent(event); }
+
+      void Update(const spitfire::math::cTimeStep& timeStep) { _Update(timeStep); }
+      void UpdateInput(const spitfire::math::cTimeStep& timeStep) { _UpdateInput(timeStep); }
+      void Render(const spitfire::math::cTimeStep& timeStep) { _Render(timeStep); }
+
+    protected:
+      render::cSystem& system;
+      render::cWindow* pWindow;
+      render::cContext* pContext;
+
+      audio::cManager* pAudioManager;
+
+      gui::cManager* pGuiManager;
+      gui::cRenderer* pGuiRenderer;
+
+      gui::cLayer* pLayer;
+
+    private:
+      virtual void _OnEnter() {}
+      virtual void _OnExit() {}
+      virtual void _OnPause() {}
+      virtual void _OnResume() {}
+
+      virtual void _OnWindowEvent(const gui::cWindowEvent& event) {}
+      virtual void _OnMouseEvent(const gui::cMouseEvent& event) {}
+      virtual void _OnKeyboardEvent(const gui::cKeyboardEvent& event) {}
+
+      virtual void _Update(const spitfire::math::cTimeStep& timeStep) {}
+      virtual void _UpdateInput(const spitfire::math::cTimeStep& timeStep) {}
+      virtual void _Render(const spitfire::math::cTimeStep& timeStep) {}
+
+
+      virtual void _OnStateMouseEvent(const gui::cMouseEvent& event) {}
+      virtual void _OnStateKeyboardEvent(const gui::cKeyboardEvent& event) {}
+
+      virtual gui::EVENT_RESULT _OnWidgetEvent(const gui::cWidgetEvent& event) { return gui::EVENT_RESULT::NOT_HANDLED_PERCOLATE; }
+    };
+
+
+    // ** cApplication
+
+    class cApplication : public opengl::cWindowEventListener, public opengl::cInputEventListener
+    {
+    public:
+      friend class cState;
+
+      cApplication();
+      ~cApplication();
+
+      void Run();
+
+      void PushStateSoon(cState* pState);
+      void PopStateSoon();
+
+    protected:
+      // State management
+      const cState* GetState() const;
+      cState* GetState();
+      void PushState(cState* pState);
+      void PopState();
+
+      bool bIsDone;
+
+      opengl::cSystem system;
+      opengl::cWindow* pWindow;
+      opengl::cContext* pContext;
+
+      breathe::audio::cManager* pAudioManager;
+
+    private:
+      bool Create();
+      void Destroy();
+
+      void MainLoop();
+
+      virtual bool _Create() { return true; }
+      virtual void _Destroy() {}
+
+      void _OnWindowEvent(const opengl::cWindowEvent& event);
+      void _OnMouseEvent(const opengl::cMouseEvent& event);
+      void _OnKeyboardEvent(const opengl::cKeyboardEvent& event);
+
+      // State event management (Transitioning between states)
+      void ProcessStateEvents();
+
+
+      std::stack<cState*> states;
+
+      // State change event queue
+      typedef cState* cStateEvent;
+      std::vector<cState*> stateEvents;
+    };
+  }
+}
+
+#endif // CAPPLICATION_H
+
+
+/*#ifndef CAPPLICATION_H
+#define CAPPLICATION_H
+
 #include <breathe/breathe.h>
 
 #include <breathe/audio/audio.h>
@@ -47,19 +184,26 @@ namespace breathe
     bool InitApp();
     bool DestroyApp();
 
+
+
     bool Run();
 
+
     void AddKeyRepeat(unsigned int code); // ie. for a key like wasd.
+
     void AddKeyNoRepeat(unsigned int code); // ie. for key like escape, enter, spacebar, etc.
     void AddKeyToggle(unsigned int code); // ie. tilde for console, either on or off, a press toggles.
 
+
     bool IsKeyDown(unsigned int code) const;
     bool IsKeyDownReset(unsigned int code) const;
+
 
     bool IsMouseLeftButtonDown() const;
     bool IsMouseRightButtonDown() const;
     bool IsMouseScrollUp() const;
     bool IsMouseScrollDown() const;
+
 
     int GetMouseX() const;
     int GetMouseY() const;
@@ -67,6 +211,7 @@ namespace breathe
     int GetMouseDeltaY() const;
 
     void CursorShow();
+
     void CursorHide();
     void CursorWarpToMiddleOfScreen();
 
@@ -74,6 +219,7 @@ namespace breathe
 
 #ifdef BUILD_DEBUG
     bool IsDebug() const { return bDebug; }
+
     void ToggleDebug() { bDebug = !bDebug; }
 
     void CreateGameUnitTest();
@@ -84,6 +230,7 @@ namespace breathe
 
 
     bool IsHDRBloomPostRenderEffect() const { return bIsHDRBloomPostRenderEffect; }
+
     void SetHDRBloomPostRenderEffect(bool _bIsHDRBloomPostRenderEffect);
 
 
@@ -94,6 +241,7 @@ namespace breathe
     gui::cWindowManager window_manager;
 
     class cAppState
+
     {
     public:
       explicit cAppState(size_t _uiState) : uiState(_uiState), result(0), pParent(nullptr) {}
@@ -103,6 +251,7 @@ namespace breathe
 
       void SetParent(cAppState* _pParent) { assert(_pParent != nullptr); pParent = _pParent; }
       cAppState* GetParent() { return pParent; }
+
 
       void SetResult(int iResult) { result = iResult; }
       int GetResult() const { return result; }
@@ -114,6 +263,7 @@ namespace breathe
       void OnExit() { _OnExit(); }
 
       // Functions that deal with states being parented by other states,
+
       // the child runs and then returns a result to the parent
       void OnPause() { _OnPause(); }
       void OnResume(int iResult) { _OnResume(iResult); }
@@ -140,6 +290,7 @@ namespace breathe
         bool IsKeyDown(key_t key) const;
         bool IsKeyUp(key_t key) const;
 
+
       private:
         std::list<key_t> keys; // This can contain for example KEY::A, KEY::SHIFT
       };
@@ -149,6 +300,7 @@ namespace breathe
       public:
         bool IsPrimaryButtonDown() const;
         bool IsSecondaryButtonDown() const;
+
         bool IsTertiaryButtonDown() const;
 
         float GetX() const;
@@ -171,6 +323,7 @@ namespace breathe
       void AddMessageError(const string_t& text) { SCREEN.AddMessageError(text); }
 
       // Which person is speaking, what they are saying
+
       void AddClosedCaption(const string_t& actor, const string_t& line, uint32_t life) { SCREEN.AddClosedCaption(actor, line, life); }
 
     private:
@@ -178,6 +331,7 @@ namespace breathe
       virtual void _OnExit() {}
       virtual void _OnPause() {}
       virtual void _OnResume(int iResult) {}
+
 
       virtual void _Update(sampletime_t currentTime) {}
       virtual void _UpdateInput(sampletime_t currentTime) {}
@@ -191,9 +345,11 @@ namespace breathe
       virtual void _RenderScene(sampletime_t currentTime) {}
       virtual void _RenderScreenSpace(sampletime_t currentTime) {}
 
+
       virtual void _OnMouseEvent(int button, int state, float x, float y) {}
 
       size_t uiState;
+
       int result;
       cAppState* pParent;
     };
@@ -201,178 +357,291 @@ namespace breathe
   protected:
     cAppState& GetCurrentState() const;
 
+
     // Instantaneous
     void PushState(cAppState* state);
+
     void PopState();
 
     // Deferred until we get back to the main cApplication loop
+
     void PushStateSoon(cAppState* state) { ASSERT(pPushThisStateSoon == nullptr); pPushThisStateSoon = state; }
     void PopStateSoon() { ASSERT(bPopCurrentStateSoon == false); bPopCurrentStateSoon = true; }
 
     // This does both, it defers changing state until we get back to the main cApplication loop,
     // but it also pops the current state and pushes a new one
+
     void PopStateAndPushNewStateSoon(cAppState* state) { PopStateSoon(); PushStateSoon(state); }
 
 #if defined(BUILD_PHYSICS_2D) || defined(BUILD_PHYSICS_3D)
     bool bStepPhysics;
+
     bool bUpdatePhysics;
 #endif
     bool bDone;
 
+
+
     SDL_Event event;
 
+
 #if defined(BUILD_PHYSICS_2D) || defined(BUILD_PHYSICS_3D)
+
     spitfire::util::cTimer tPhysics;
+
 #endif
     spitfire::util::cTimer tUpdate;
     spitfire::util::cTimer tRender;
 
 
+
+
     render::cSystem system;
+
     render::cContext* pContext;
+
     render::cWindow* pWindow;
     render::cResourceManager* pResourceManager;
 
+
+
     render::cFont* pFont;
+
+
 
     render::cCamera camera;
 
+
     scenegraph3d::cSceneGraph scenegraph;
+
     scenegraph2d::cSceneGraph scenegraph2D;
+
 
     breathe::audio::cManager* pAudioManager;
 
+
 #if defined(BUILD_PHYSICS_2D) || defined(BUILD_PHYSICS_3D)
+
     breathe::physics::cWorld* pWorld;
+
 #endif
+
+
 
     std::vector<SDL_Joystick*> vJoystick;
 
+
+
   private:
+
     class cConsoleWindow : public gui::cModelessWindow
     {
+
     public:
+
       cConsoleWindow();
 
       void InitConsoleWindow();
 
+
       gui::cWidget_StaticText& GetPrevious() { return *pPrevious; }
+
       gui::cWidget_Input& GetInput() { return *pInput; }
+
 
     private:
       void _OnMouseEvent(int button, int state, int x, int y) {}
+
       void _OnEvent(const gui::cEvent& event);
+
 
       gui::cWidget_StaticText* pPrevious;
       gui::cWidget_Input* pInput;
+
     };
 
     class cKey
+
     {
     public:
       cKey(unsigned int code, bool variable, bool repeat, bool toggle);
 
+
       bool IsDown() const;
       void SetDown(bool bConsole);
+
       void SetUp(bool bConsole);
+
 
       //std::string sCommand;
       //cKey(const std::string& command);
 
       bool bVariable;
+
       bool bRepeat;
       bool bToggle;
 
+
       unsigned int uiCode;
+
 
       mutable bool bDown;
       mutable bool bCollected;
     };
 
+
     class cMouse
     {
+
     public:
       cMouse() : x(0), y(0) { down.reset(); }
+
 
       bool IsDown(bool bConsole, size_t button) const { assert(button < 10); return down[button]; }
       bool IsUp(bool bConsole, size_t button) const { assert(button < 10); return !down[button]; }
 
       int GetX() const { return x; }
+
       int GetY() const { return y; }
 
+
       std::bitset<10> down;
+
       int x;
+
       int y;
+
     };
 
+
+
     void LoadConfigXML(render::cResolution& resolution);
+
     void _ConsoleExecuteSingleCommand(const std::string& s);
+
     void _InitArguments(int argc, const char* const* argv);
+
     void _LoadSearchDirectories();
 
+
+
     // Pure virtual functions, these *have* to be overridden in your derived game class
+
     virtual bool LoadScene() = 0;
     virtual bool InitScene() = 0;
+
     virtual bool DestroyScene() = 0;
 
+
     virtual void FullscreenSwitch()=0;
+
     virtual bool Execute(const std::string& sCommand)=0;
 
+
     // Convert from a float amount to a bool
+
     static bool _IsKeyDown(float fAmount) { return (fAmount > KEY_MIN || fAmount < -KEY_MIN); }
 
+
+
     void _UpdateKeys(sampletime_t currentTime);
+
     void _UpdateEvents(sampletime_t currentTime);
 
+
+
     void _OnKeyUp(SDL_keysym* keysym);
+
     void _OnKeyDown(SDL_keysym* keysym);
+
     void _OnMouseUp(int button, int x, int y);
+
     void _OnMouseDown(int button, int x, int y);
+
     void _OnMouseMove(int button, int x, int y);
 
 
+
+
+
     void _Update(sampletime_t currentTime);
+
     void _UpdateInput(sampletime_t currentTime);
+
 #if defined(BUILD_PHYSICS_2D) || defined(BUILD_PHYSICS_3D)
+
     void _UpdatePhysics(cApplication::cAppState& state, sampletime_t currentTime);
+
 #endif
 
+
     // Main rendering function, basically controls the rendering process from rendering to textures, using those textures to render a scene and applying those textures to the screen
+
     void _Render(cApplication::cAppState& state, sampletime_t currentTime);
+
     void _RenderScreenSpaceScene(cApplication::cAppState& state, sampletime_t currentTime);
 
 
+
+
+
 #ifdef BUILD_DEBUG
+
     bool bDebug;
+
 #endif
 
+
+
     bool bIsBlurPostRenderEffect;
+
     bool bIsHDRBloomPostRenderEffect;
 
+
+
     bool bActive;
+
     bool bReturnCode;
+
+
 
     std::vector<std::string>vArgs;
 
+
     std::map<unsigned int, cKey* >mKey;
+
     cMouse mouse;
 
+
+
     // TODO: Change to std::stack?
+
     std::list<cAppState*> states;
 
 
+
+
   private:
+
 #ifdef BUILD_DEBUG
+
     void SanityCheck();
 #endif
 
+
+
     void CheckIfNeedToUpdateApplication();
+
 
     void CreateFBOTextures();
 
+
+
     void RemoveKey(unsigned int code);
 
+
     virtual cAppState* _GetFirstAppState() = 0;
+
 
     // Forbidden
     void _OnMouseEvent(int button, int state, int x, int y);
@@ -421,4 +690,5 @@ namespace breathe
   };
 }
 
-#endif // CAPPLICATION_H
+#endif // CAPPLICATION_H*/
+
--- src/breathe/util/cApplication.cpp
+++ src/breathe/util/cApplication.cpp
@@ -47,47 +47,318 @@ int intersect_triangle(
   *u = tvec.DotProduct(pvec);
   if (*u < 0.0 || *u > det) return 0;
 
+
   // prepare to test V parameter
   qvec = tvec.CrossProduct(edge1);
 
   // calculate V parameter and test bounds
+
   *v = dir.DotProduct(qvec);
   if (*v < 0.0 || *u + *v > det) return 0;
 
   // calculate t, scale parameters, ray intersects triangle
   *t = edge2.DotProduct(qvec);
+
   double inv_det = 1.0 / det;
   *t *= inv_det;
   *u *= inv_det;
   *v *= inv_det;
+
 
 #else                    // the non-culling branch
    if (det > -EPSILON && det < EPSILON) return 0;
 
    double inv_det = 1.0 / det;
 
+
    // calculate distance from vert0 to ray origin
    tvec = orig - vert0;
 
    // calculate U parameter and test bounds
    *u = tvec.DotProduct(pvec) * inv_det;
+
    if (*u < 0.0 || *u > 1.0) return 0;
 
    // prepare to test V parameter
    qvec = tvec.CrossProduct(edge1);
 
    // calculate V parameter and test bounds
+
    *v = dir.DotProduct(qvec) * inv_det;
 
    if (*v < 0.0 || *u + *v > 1.0) return 0;
 
    // calculate t, ray intersects triangle
+
    *t = edge2.DotProduct(qvec) * inv_det;
 #endif
 
    return 1;
 }
+
 */
+
+#include <breathe/util/cApplication.h>
+
+namespace breathe
+{
+  namespace util
+  {
+    // ** cState
+
+    cState::cState(cApplication& application) :
+      system(application.system),
+      pWindow(application.pWindow),
+      pContext(application.pContext),
+      pAudioManager(application.pAudioManager)
+    {
+    }
+
+    cState::~cState()
+    {
+    }
+
+
+    // ** cApplication
+
+    cApplication::cApplication() :
+      bIsDone(false),
+
+      pWindow(nullptr),
+      pContext(nullptr),
+
+      pAudioManager(nullptr)
+    {
+    }
+
+    cApplication::~cApplication()
+    {
+      Destroy();
+    }
+
+    bool cApplication::Create()
+    {
+      const opengl::cCapabilities& capabilities = system.GetCapabilities();
+
+      opengl::cResolution resolution = capabilities.GetCurrentResolution();
+      if ((resolution.width < 1024) || (resolution.height < 768) || (resolution.pixelFormat != opengl::PIXELFORMAT::R8G8B8A8)) {
+        std::cout<<"Current screen resolution is not adequate "<<resolution.width<<"x"<<resolution.height<<std::endl;
+        return false;
+      }
+
+      // Set our required resolution
+      resolution.width = 1024;
+      resolution.height = 768;
+      resolution.pixelFormat = opengl::PIXELFORMAT::R8G8B8A8;
+
+      pWindow = system.CreateWindow(TEXT("Tetris"), resolution, false);
+      if (pWindow == nullptr) {
+        std::cout<<"Window could not be created"<<std::endl;
+        return false;
+      }
+
+      pContext = pWindow->GetContext();
+      if (pContext == nullptr) {
+        std::cout<<"Context could not be created"<<std::endl;
+        return false;
+      }
+
+      // Setup our event listeners
+      pWindow->SetWindowEventListener(*this);
+      pWindow->SetInputEventListener(*this);
+
+      pAudioManager = breathe::audio::Create(breathe::audio::DRIVER::SDLMIXER);
+
+
+      // Call our overridden create function
+      return _Create();
+    }
+
+    void cApplication::Destroy()
+    {
+      // Call our overridden destroy function
+      _Destroy();
+
+
+      if (pAudioManager != nullptr) {
+        breathe::audio::Destroy(pAudioManager);
+        pAudioManager = nullptr;
+      }
+
+      pContext = nullptr;
+
+      if (pWindow != nullptr) {
+        system.DestroyWindow(pWindow);
+        pWindow = nullptr;
+      }
+    }
+
+    void cApplication::_OnWindowEvent(const opengl::cWindowEvent& event)
+    {
+      std::cout<<"cApplication::_OnWindowEvent"<<std::endl;
+
+      if (event.IsQuit()) {
+        std::cout<<"cApplication::_OnWindowEvent Quiting"<<std::endl;
+        bIsDone = true;
+      }
+    }
+
+    void cApplication::_OnMouseEvent(const opengl::cMouseEvent& event)
+    {
+      cState* pState = GetState();
+      if (pState != nullptr) pState->OnMouseEvent(event);
+    }
+
+    void cApplication::_OnKeyboardEvent(const opengl::cKeyboardEvent& event)
+    {
+      cState* pState = GetState();
+      if (pState != nullptr) pState->OnKeyboardEvent(event);
+    }
+
+    void cApplication::PushStateSoon(cState* pState)
+    {
+      cStateEvent event = pState;
+      stateEvents.push_back(event);
+    }
+
+    void cApplication::PopStateSoon()
+    {
+      cStateEvent event = nullptr; // A null state event means "Pop the current state"
+      stateEvents.push_back(event);
+    }
+
+    void cApplication::PushState(cState* pState)
+    {
+      ASSERT(pState != nullptr);
+
+      cState* pOldState = GetState();
+      if (pOldState != nullptr) pOldState->Pause();
+
+      states.push(pState);
+
+      pState->Resume();
+    }
+
+    void cApplication::PopState()
+    {
+      cState* pOldState = GetState();
+      if (pOldState != nullptr) {
+        pOldState->Pause();
+
+        // Delete and remove the old state
+        delete pOldState;
+        states.pop();
+
+        // Prepare the previous state
+        cState* pCurrentState = GetState();
+        if (pCurrentState != nullptr) pCurrentState->Resume();
+      }
+    }
+
+    cState* cApplication::GetState()
+    {
+      cState* pState = nullptr;
+      if (!states.empty()) {
+        pState = states.top();
+        ASSERT(pState != nullptr);
+      }
+      return pState;
+    }
+
+    void cApplication::ProcessStateEvents()
+    {
+      const size_t n = stateEvents.size();
+      for (size_t i = 0; i < n; i++) {
+        cState* pState = stateEvents[i];
+        if (pState != nullptr) PushState(pState);
+        else PopState(); // A null state event means "Pop the current state"
+      }
+
+      stateEvents.clear();
+    }
+
+    void cApplication::MainLoop()
+    {
+      assert(pContext != nullptr);
+      assert(pContext->IsValid());
+
+      uint32_t T0 = 0;
+      uint32_t Frames = 0;
+
+      uint32_t currentTime = SDL_GetTicks();
+      uint32_t lastTime = SDL_GetTicks();
+
+      uint32_t lastUpdateTime = SDL_GetTicks();
+
+      while (!states.empty()) {
+        ProcessStateEvents();
+        if (states.empty()) break;
+
+        // Update window events
+        pWindow->UpdateEvents();
+
+        // Update state
+        lastTime = currentTime;
+        currentTime = SDL_GetTicks();
+        {
+          const spitfire::math::cTimeStep timeStep(currentTime, currentTime - lastTime);
+          cState* pState = GetState();
+          assert(pState != nullptr);
+          pState->UpdateInput(timeStep);
+        }
+
+        // Perform an Update, these are locked at 30 fps
+        if ((currentTime - lastUpdateTime) > 33) {
+          const spitfire::math::cTimeStep timeStep(currentTime, 33);
+          cState* pState = GetState();
+          assert(pState != nullptr);
+          pState->Update(timeStep);
+          lastUpdateTime = currentTime;
+        }
+
+        // Update audio
+        const spitfire::math::cVec3 listenerPosition;
+        const spitfire::math::cVec3 listenerTarget;
+        const spitfire::math::cVec3 listenerUp(0.0f, 0.0f, 1.0f);
+        pAudioManager->Update(currentTime, listenerPosition, listenerTarget, listenerUp);
+
+        // Render a frame
+        {
+          const spitfire::math::cTimeStep timeStep(currentTime, currentTime - lastTime);
+          cState* pState = GetState();
+          assert(pState != nullptr);
+          pState->Render(timeStep);
+        }
+
+        // Gather our frames per second
+        Frames++;
+        {
+          const uint32_t t = SDL_GetTicks();
+          // TODO: Try changing this to 1000
+          if (t - T0 >= 5000) {
+            const float seconds = (t - T0) / 1000.0;
+            const float fps = Frames / seconds;
+            std::cout<<Frames<<" frames in "<<seconds<<" seconds = "<<fps<<" FPS"<<std::endl;
+            T0 = t;
+            Frames = 0;
+          }
+        }
+      };
+    }
+
+    void cApplication::Run()
+    {
+      const bool bIsSuccess = Create();
+      if (bIsSuccess) MainLoop();
+
+      Destroy();
+    }
+  }
+}
+
+
+
+#if 0
 
 
 // Wii Fit Linux
@@ -2230,3 +2501,6 @@ namespace breathe
     }
   }
 }
+
+#endif
+
