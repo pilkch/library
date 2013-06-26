7ee947eda26e476b2f04127c9320230292e5abca
Chris Pilkington <chris.pilkington@gmail.com>
2013-06-26 23:44:01 +1000

    spitfire Added update checker.

---
+++ include/spitfire/util/updatechecker.h
@@ -0,0 +1,41 @@
+#ifndef UPDATECHECKER_H
+#define UPDATECHECKER_H
+
+// Spitfire headers
+#include <spitfire/communication/http.h>
+#include <spitfire/util/signalobject.h>
+#include <spitfire/util/thread.h>
+
+namespace spitfire
+{
+  namespace util
+  {
+    class cUpdateCheckerHandler
+    {
+    public:
+      virtual ~cUpdateCheckerHandler() {}
+
+      virtual void OnNewVersionFound(int iMajorVersion, int iMinorVersion, const string_t& sDownloadPage) = 0;
+    };
+
+    class cUpdateChecker : public spitfire::util::cThread, public spitfire::network::http::cRequestListener
+    {
+    public:
+      explicit cUpdateChecker(cUpdateCheckerHandler& handler);
+
+    private:
+      // For the request listener
+      virtual bool _IsToStop() const override;
+      virtual void _OnStatusReceived(spitfire::network::http::STATUS status) override;
+      virtual void _OnTextContentReceived(const std::string& sContent) override;
+
+      virtual void ThreadFunction() override;
+
+      cUpdateCheckerHandler& handler;
+
+      spitfire::util::cSignalObject soAction;
+    };
+  }
+}
+
+#endif // UPDATECHECKER_H
---
+++ src/spitfire/util/updatechecker.cpp
@@ -0,0 +1,115 @@
+// Spitfire headers
+#include <spitfire/storage/xml.h>
+#include <spitfire/util/updatechecker.h>
+
+namespace spitfire
+{
+  namespace util
+  {
+    cUpdateChecker::cUpdateChecker(cUpdateCheckerHandler& _handler) :
+      spitfire::util::cThread(soAction, "cUpdateChecker"),
+      handler(_handler),
+      soAction("soAction")
+    {
+    }
+
+    bool cUpdateChecker::_IsToStop() const
+    {
+      return spitfire::util::cThread::IsToStop();
+    }
+
+    void cUpdateChecker::_OnStatusReceived(spitfire::network::http::STATUS status)
+    {
+      if (status != spitfire::network::http::STATUS::ACCEPTED) {
+        LOG<<"cUpdateChecker::_OnStatusReceived Bad status received "<<spitfire::network::http::GetStatusAsString(status)<<" "<<spitfire::network::http::GetStatusDescription(status)<<std::endl;
+        StopThreadSoon();
+      }
+    }
+
+    string_t GetContentOfChildNode(spitfire::document::cNode::const_iterator& parent, const string_t& sChildName)
+    {
+      // Get the child
+      spitfire::document::cNode::const_iterator iterChild(parent.GetChild(sChildName));
+      if (!iterChild.IsValid()) {
+        LOG<<"GetContentOfChildNode Child \""<<sChildName<<"\" not found, returning \"\""<<std::endl;
+        return "";
+      }
+
+      // Get the content
+      spitfire::document::cNode::const_iterator iterContent(iterChild.GetFirstChild());
+      if (!iterContent.IsValid()) {
+        LOG<<"GetContentOfChildNode Child \""<<sChildName<<"\" content node not found, returning \"\""<<std::endl;
+        return "";
+      }
+
+      return iterContent.GetContent();
+    }
+
+    void cUpdateChecker::_OnTextContentReceived(const std::string& sContent)
+    {
+      LOG<<"cUpdateChecker::_OnTextContentReceived"<<std::endl;
+
+      // Read the xml document
+      spitfire::xml::reader reader;
+      spitfire::document::cDocument document;
+
+      spitfire::util::cProcessInterfaceVoid interface;
+      spitfire::util::PROCESS_RESULT result = reader.ReadFromString(interface, document, sContent);
+      if (result != spitfire::util::PROCESS_RESULT::COMPLETE) {
+        LOG<<"cUpdateChecker::_OnTextContentReceived ReadFromString failed for string \""<<sContent<<"\", returning"<<std::endl;
+        return;
+      }
+
+      //<latest>
+      //  <linux>
+      //    <version>0.8</version>
+      //    <download>http://chris.iluo.net/projects/medusa/medusa.tar.gz</download>
+      //    <page>http://chris.iluo.net/projects/medusa/</page>
+      //  </linux>
+      //</latest>
+
+      spitfire::document::cNode::const_iterator iterLatest(document);
+      if (!iterLatest.IsValid()) return;
+
+      iterLatest.FindChild("latest");
+      if (!iterLatest.IsValid()) return;
+
+      spitfire::document::cNode::const_iterator iterPlatform(iterLatest);
+      if (!iterPlatform.IsValid()) return;
+
+      iterPlatform.FindChild("linux");
+      if (!iterPlatform.IsValid()) return;
+
+      // Get the version
+      const string_t sVersion = GetContentOfChildNode(iterPlatform, "version");
+      if (sVersion.empty()) {
+        LOG<<"cUpdateChecker::_OnTextContentReceived Could not find version node or content, returning"<<std::endl;
+        return;
+      }
+
+      spitfire::string::cStringParser sp(sVersion);
+      string_t sMajorVersion;
+      sp.GetToStringAndSkip(".", sMajorVersion);
+      const string_t sMinorVersion = sp.GetToEnd();
+      const int iMajorVersion = spitfire::string::ToUnsignedInt(sMajorVersion);
+      const int iMinorVersion = spitfire::string::ToUnsignedInt(sMinorVersion);
+
+
+      // Get the page
+      const string_t sDownloadPage = GetContentOfChildNode(iterPlatform, "page");
+      if (sDownloadPage.empty()) {
+        LOG<<"cUpdateChecker::_OnTextContentReceived Could not find page node or content, returning"<<std::endl;
+        return;
+      }
+
+      // Notify the handler
+      handler.OnNewVersionFound(iMajorVersion, iMinorVersion, sDownloadPage);
+    }
+
+    void cUpdateChecker::ThreadFunction()
+    {
+      spitfire::network::http::cHTTP connection;
+      connection.Download(TEXT(BUILD_APPLICATION_WEBSITE) TEXT("version.xml"), spitfire::network::http::METHOD::GET, *this);
+    }
+  }
+}
