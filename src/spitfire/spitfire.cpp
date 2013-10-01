c376fd04c81ce960c669e3766fa68f346c9fade7
Chris Pilkington <chris.pilkington@gmail.com>
2013-10-01 21:17:41 +1000

    spitfire Compiled.

--- src/spitfire/spitfire.cpp
+++ src/spitfire/spitfire.cpp
@@ -65,21 +65,21 @@ namespace spitfire
     return o.str();
   }
 
-  /*void InformativeAssert(bool bIsResultTrue, const char* szAssert, const char* szFile, const char* szFunction, size_t line)
+  void InformativeAssert(bool bIsResultTrue, const char* szAssert, const char* szFile, const char* szFunction, size_t line)
   {
     if (!bIsResultTrue) {
-      bool bIsLogging = logging::IsLogging();
+      //bool bIsLogging = logging::IsLogging();
 
       // Make sure that we log this assert even if logging is off
-      logging::TurnOnLogging();
+      //logging::TurnOnLogging();
 
       std::cout<<"ASSERTION FAILED "<<szAssert<<" "<<szFile<<" "<<szFunction<<":"<<line<<std::endl;
       LOG<<"ASSERTION FAILED "<<szAssert<<" "<<szFile<<" "<<szFunction<<":"<<line<<std::endl;
       assert(bIsResultTrue);
 
       // If logging was off then keep it off
-      if (!bIsLogging) logging::TurnOffLogging();
+      //if (!bIsLogging) logging::TurnOffLogging();
     }
-  }*/
+  }
 #endif
 }
