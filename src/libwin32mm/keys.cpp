9fb122ba9f48ec1fa0473a39a15f650d18b607ae
Chris Pilkington <chris.pilkington@gmail.com>
2014-03-17 23:27:51 +1100

    libwin32mm keys Simplified.

--- src/libwin32mm/keys.cpp
+++ src/libwin32mm/keys.cpp
@@ -19,31 +19,21 @@ namespace win32mm
 
   string_t KeyGetDescription(key_t key)
   {
+    const uint16_t uVirtKey = uint16_t(key);
+    uint32_t uScanCode = ::MapVirtualKey(uVirtKey, 0);
+
+    // Set the extended Key flag for keys which are apart of the extended keyboard
+    // NOTE: There is a conflict for keys with the same function coming from the extended keyboard so we need to make sure that the flag is set
+    const key_t keyNoMod = KeyRemoveModFlags(key);
+    if ((keyNoMod == KEY_LEFT) || (keyNoMod == KEY_RIGHT) || (keyNoMod == KEY_UP) || (keyNoMod == KEY_DOWN) || (keyNoMod == KEY_INSERT) ||
+      (keyNoMod == KEY_PGUP) || (keyNoMod == KEY_PGDN) || (keyNoMod == KEY_DELETE) || (keyNoMod == KEY_HOME) || (keyNoMod == KEY_END)) {
+      uScanCode |= 0x100;
+    }
+
     char_t szKeyName[KEY_NAME_LENGTH];
     szKeyName[0] = 0;
 
-    #ifdef LANGUAGE_JAPANESE
-    // Provide the correct Japanese name for Left Arrow and Rigth Arrow keys
-    const key_t keyJapanese = key & ~(KEY_MOD_CONTROL | KEY_MOD_ALT | KEY_MOD_SHIFT);
-    if (keyJapanese == KEY_LEFT) tstrcpy(szKeyName, LANGTAG("Left Arrow"));
-    else if (keyJapanese == KEY_RIGHT) tstrcpy(szKeyName, LANGTAG("Right Arrow"));
-    #endif
-
-    const key_t keyNoMod = KeyRemoveModFlags(key);
-
-    if (szKeyName[0] == 0) {
-      const uint16_t uVirtKey = uint16_t(key);
-      uint32_t uScanCode = ::MapVirtualKey(uVirtKey, 0);
-
-      // Set the extended Key flag for keys which are apart of the extended keyboard
-      // NOTE: There is a conflict for keys with the same function coming from the extended keyboard so we need to make sure that the flag is set
-      if ((keyNoMod == KEY_LEFT) || (keyNoMod == KEY_RIGHT) || (keyNoMod == KEY_UP) || (keyNoMod == KEY_DOWN) || (keyNoMod == KEY_INSERT) ||
-        (keyNoMod == KEY_PGUP) || (keyNoMod == KEY_PGDN) || (keyNoMod == KEY_DELETE) || (keyNoMod == KEY_HOME) || (keyNoMod == KEY_END)) {
-        uScanCode |= 0x100;
-      }
-
-      ::GetKeyNameText(MAKELPARAM(uVirtKey, uScanCode), szKeyName, KEY_NAME_LENGTH);
-    }
+    ::GetKeyNameText(MAKELPARAM(uVirtKey, uScanCode), szKeyName, KEY_NAME_LENGTH);
 
     // Special case for the pause key
     if ((szKeyName[0] == 0) && (keyNoMod == KEY_PAUSE)) tstrcpy(szKeyName, LANGTAG("Pause"));
