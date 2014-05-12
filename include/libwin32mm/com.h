daba2bcdab82915ffa4d9ecadeeb777738df6369
Chris Pilkington <chris.pilkington@gmail.com>
2014-05-12 23:13:00 +1000

    __WIN__ libwin32mm Added com.

--- include/libwin32mm/com.h
+++ include/libwin32mm/com.h
@@ -1,6 +1,10 @@
 #ifndef LIBWIN32MM_COM_H
 #define LIBWIN32MM_COM_H
 
+// Standard headers
+#include <iostream>
+
+// Windows headers
 #include <windows.h>
 #include <objbase.h>
 
@@ -22,6 +26,51 @@ namespace win32mm
     bool bInitialised;
   };
 
+
+  // ** cComObject
+
+  template <class T>
+  class cComObject {
+  public:
+    cComObject();
+    ~cComObject();
+
+    void CreateObject(REFCLSID rclsid, REFIID riid);
+
+    bool IsValid() const;
+
+    const T& operator*() const;
+    T& operator*();
+    const T* operator->() const;
+    T* operator->();
+
+  private:
+    NO_COPY(cComObject<T>);
+
+    void Release();
+
+    T* p;
+  };
+
+
+  // ** COM_SAFE_RELEASE
+  //
+  // For releasing a COM object and setting it to NULL so that nothing else tries to use it
+
+  template <class T>
+  inline void COM_SAFE_RELEASE(T& x)
+  {
+    ASSERT(x != nullptr);
+    x->Release();
+    x = nullptr;
+  }
+
+
+
+  // ** Inlines
+
+  // ** cComScope
+
   inline cComScope::cComScope() :
     bInitialised(false)
   {
@@ -36,16 +85,75 @@ namespace win32mm
   }
 
 
-  // ** COM_SAFE_RELEASE
-  //
-  // For releasing a COM object and setting it to NULL so that nothing else tries to use it
+  // ** cComObject
 
   template <class T>
-  inline void COM_SAFE_RELEASE(T& x)
+  inline cComObject<T>::cComObject() :
+    p(nullptr)
   {
-    ASSERT(x != nullptr);
-    x->Release();
-    x = nullptr;
+  }
+
+  template <class T>
+  inline cComObject<T>::~cComObject()
+  {
+     Release();
+  }
+
+  template <class T>
+  inline void cComObject<T>::Release()
+  {
+     if (p != nullptr) {
+        p->Release();
+        p = nullptr;
+     }
+  }
+
+  template <class T>
+  inline bool cComObject<T>::IsValid() const
+  {
+     return (p != nullptr);
+  }
+
+  template <class T>
+  inline const T& cComObject<T>::operator*() const
+  {
+     ASSERT(p != nullptr);
+     return *p;
+  }
+
+  template <class T>
+  inline T& cComObject<T>::operator*()
+  {
+     ASSERT(p != nullptr);
+     return *p;
+  }
+
+  template <class T>
+  inline const T* cComObject<T>::operator->() const
+  {
+     ASSERT(p != nullptr);
+     return p;
+  }
+
+
+  template <class T>
+  inline T* cComObject<T>::operator->()
+  {
+     ASSERT(p != nullptr);
+     return p;
+  }
+
+  template <class T>
+  inline void cComObject<T>::CreateObject(REFCLSID rclsid, REFIID riid)
+  {
+    // Release any existing object
+    Release();
+
+    // Create our new object
+    const HRESULT result = CoCreateInstance(rclsid, NULL, CLSCTX_INPROC_SERVER, riid, (void**)&p);
+    if (result != S_OK) {
+      std::wcerr<<TEXT("cComObject::CreateObject CoCreateInstance FAILED result=")<<result<<TEXT(", last error=")<<GetLastError()<<std::endl;
+    }
   }
 }
 
