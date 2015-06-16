8a75750b3e1e5be043b8b5f928affa2b82e9fc2f
Chris Pilkington <chris.pilkington@gmail.com>
2015-06-17 00:01:23 +1000

    libopenglmm Added char_t.

--- include/libopenglmm/libopenglmm.h
+++ include/libopenglmm/libopenglmm.h
@@ -59,8 +59,10 @@ namespace opengl
 {
   #ifdef UNICODE
   typedef std::wstring string_t;
+  typedef wchar_t char_t;
   #else
   typedef std::string string_t;
+  typedef char char_t;
   #endif
 
   namespace string
