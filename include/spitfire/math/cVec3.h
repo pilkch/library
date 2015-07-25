2b2ce11e6d78388c06cf10e7566664239e682dee
Chris Pilkington <chris.pilkington@gmail.com>
2015-07-25 23:03:08 +1000

    spitfire math vec3 Fixed front and back vectors.

--- include/spitfire/math/cVec3.h
+++ include/spitfire/math/cVec3.h
@@ -152,8 +152,8 @@ namespace spitfire
     const cVec3 v3Down(0.0f, -1.0f, 0.0f);
     const cVec3 v3Left(-1.0f, 0.0f, 0.0f);
     const cVec3 v3Right(1.0f, 0.0f, 0.0f);
-    const cVec3 v3Front(0.0f, 0.0f, 1.0f);
-    const cVec3 v3Back(0.0f, 0.0f, -1.0f);
+    const cVec3 v3Front(0.0f, 0.0f, -1.0f);
+    const cVec3 v3Back(0.0f, 0.0f, 1.0f);
   }
 }
 
