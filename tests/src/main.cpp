69d297f4bbda374edc888c21586e7aa98f2ceadb
Chris Pilkington <chris.pilkington@gmail.com>
2024-11-16 22:22:56 +1100

    Compiled.

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -9,8 +9,6 @@ int main(int argc, char** argv)
 
   ::testing::InitGoogleTest(&argc, argv);
 
-  // The settings functions haven't been implemented yet
-
   // The BOM API has changed, it now requires signing up and an API key:
   // Response JSON: {"metadata":{"response_timestamp":"2024-05-05T06:26:49Z","copyright":"This Application Programming Interface (API) is owned by the Bureau of Meteorology (Bureau). You must not use, copy or share it. Please contact us for more information on ways in which you can access our data. Follow this link http://www.bom.gov.au/inside/contacts.shtml to view our contact details."},"data":[]}
   testing::GTEST_FLAG(filter) = "-SpitfireStorage.TestSettings:Weather.TestBOMRequestLocationsQuery";
